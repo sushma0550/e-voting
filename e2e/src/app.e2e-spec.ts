0831c916f0dcaa0cada3f0d4eff48981dc371d11
sushma0550 <31856143+sushma0550@users.noreply.github.com>
2021-07-20 14:18:29 +0100

    Update app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,7 +7,6 @@ describe('workspace-project App', () => {
   beforeEach(() => {
     page = new AppPage();
   });
-
   it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('e-voting app is running!');
